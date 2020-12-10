5b03e6f8225e2c8e013ae7c662e96c700af9154f
hectoriko <hectoriko@gmail.com>
2020-12-10 18:14:40 +0100

    avance en clase descartar

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
+var palabraAdivinar = 'TECLADO';
+var huecos = document.getElementById('huecos');
+
+var jugar = () => {
+  do {
+    
+  } while (i < palabraAdivinar.length);
 
 
-      var palabraAdivinar = 'TECLADO';
-      var huecos = document.getElementById('huecos');
+  for (let i = 0; i < palabraAdivinar.length; i++) {
+    
+  }
+};
+console.log('hola');
+var intento1 = prompt('Elige una letra');
 
-      var dibujarEspacios = () => {
-        for (let i = 0; i < palabraAdivinar.length; i++) {
-         huecos.innerHTMl = '_ '
-        }
-      };
 
-      var intento1 = prompt('Elige una letra');
-      dibujarEspacios();
-
-      // var intento2 = prompt('Elige una letra');
-      // var intento3 = prompt('Elige una letra');
-      // var intento4 = prompt('Elige una letra');
-      // var intento5 = prompt('Elige una letra');
+// var intento2 = prompt('Elige una letra');
+// var intento3 = prompt('Elige una letra');
+// var intento4 = prompt('Elige una letra');
+// var intento5 = prompt('Elige una letra');
