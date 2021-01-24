3be8710a576f1ad1b2fdf521561be13ed239b270
hectoriko <hectoriko@gmail.com>
2021-01-24 18:27:32 +0100

    Vuelvo a version 1

--- Dado/script.js
+++
@@ -1,9 +0,0 @@
-const tirarDado = () => {
-	let resultadoDado = Math.ceil(Math.random()*6);
-	let posicionDado = document.getElementById('dado');
-	return posicionDado.innerHTML = `<div class="dado" id="dado${resultadoDado}"></div><p>Ha salido un: ${resultadoDado}</p>`;
-}
-
-let boton = document.getElementById('boton');
-
-boton.addEventListener('click', tirarDado);
